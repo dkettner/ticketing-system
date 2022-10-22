0472164314af9527798630be0a166f58d0edcbc6
dkettner <kettner_dennis@web.de>
2022-10-22 17:04:19 +0200

    refactor(b-project): remove unused imports

--- backend/src/main/java/com/kett/TicketSystem/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/kett/TicketSystem/project/repository/ProjectRepository.java
@@ -4,7 +4,6 @@ import com.kett.TicketSystem.project.domain.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.beans.JavaBean;
 import java.util.UUID;
 
 @Repository
