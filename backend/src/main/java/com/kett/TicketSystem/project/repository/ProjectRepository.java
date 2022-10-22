26e7fc852dfc041033827659cd6187c6e6c9c18e
dkettner <kettner_dennis@web.de>
2022-10-22 14:40:41 +0200

    feat(b-project): add project repository

---
+++ backend/src/main/java/com/kett/TicketSystem/project/repository/ProjectRepository.java
@@ -0,0 +1,11 @@
+package com.kett.TicketSystem.project.repository;
+
+import com.kett.TicketSystem.project.domain.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, UUID> {
+}
