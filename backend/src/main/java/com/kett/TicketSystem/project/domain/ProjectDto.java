024ddc6eaf42bc1e1f36bbbb9c62522672306790
dkettner <kettner_dennis@web.de>
2022-10-22 18:06:14 +0200

    feat(b-project): add ProjectDto

---
+++ backend/src/main/java/com/kett/TicketSystem/project/domain/ProjectDto.java
@@ -0,0 +1,20 @@
+package com.kett.TicketSystem.project.domain;
+
+import lombok.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectDto {
+    private UUID id;
+    private String name;
+    private String description;
+    private UUID creatorId;
+    private LocalDateTime creationTime;
+    private List<UUID> memberIds = new ArrayList<>();
+}
