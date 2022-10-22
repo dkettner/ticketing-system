283661296c4a85f954ddb328df28f5121ba1069e
dkettner <kettner_dennis@web.de>
2022-10-22 13:14:09 +0200

    feat(backend): add spring boot project with maven dependencies

---
+++ src/main/java/com/kett/TicketSystem/TicketSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kett.TicketSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketSystemApplication.class, args);
+	}
+
+}
