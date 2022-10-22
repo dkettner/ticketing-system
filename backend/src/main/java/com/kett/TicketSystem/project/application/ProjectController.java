75cccb55d9c5e77a22013d38aed29694474bb7ba
dkettner <kettner_dennis@web.de>
2022-10-22 17:13:41 +0200

    feat(b-project): add exception handlers to controllers

--- backend/src/main/java/com/kett/TicketSystem/project/application/ProjectController.java
+++ backend/src/main/java/com/kett/TicketSystem/project/application/ProjectController.java
@@ -2,13 +2,12 @@ package com.kett.TicketSystem.project.application;
 
 import com.kett.TicketSystem.TicketSystemService;
 import com.kett.TicketSystem.project.domain.Project;
+import com.kett.TicketSystem.project.domain.ProjectException;
+import com.kett.TicketSystem.project.domain.ProjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -26,5 +25,17 @@ public class ProjectController {
     public ResponseEntity<Project> getProjectById(@PathVariable UUID id) {
         Project project = ticketSystemService.getProjectById(id);
         return new ResponseEntity<>(project, HttpStatus.OK);
+    }
+
+    // Exception Handlers
+
+    @ExceptionHandler(ProjectException.class)
+    public ResponseEntity<String> handleProjectException(ProjectException projectException) {
+        return new ResponseEntity<>(projectException.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ProjectNotFoundException.class)
+    public ResponseEntity<String> handleProjectNotFoundException(ProjectNotFoundException projectNotFoundException) {
+        return new ResponseEntity<>(projectNotFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
