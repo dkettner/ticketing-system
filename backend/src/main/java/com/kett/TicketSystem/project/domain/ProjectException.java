ec30c5bc43b4764b65bb72fd8a3b835b38760ddd
dkettner <kettner_dennis@web.de>
2022-10-22 14:43:17 +0200

    feat(b-project): add custom ProjectException

---
+++ backend/src/main/java/com/kett/TicketSystem/project/domain/ProjectException.java
@@ -0,0 +1,7 @@
+package com.kett.TicketSystem.project.domain;
+
+public class ProjectException extends RuntimeException {
+    public ProjectException(String message) {
+        super(message);
+    }
+}
