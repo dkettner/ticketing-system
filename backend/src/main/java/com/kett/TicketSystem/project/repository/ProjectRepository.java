68ed6604af100b2db4044d1d8fad40c1e7ed65af
dkettner <kettner_dennis@web.de>
2022-10-22 16:39:04 +0200

    refactor(b-project): use CrudRepository instead of JpaRepository

--- backend/src/main/java/com/kett/TicketSystem/project/repository/ProjectRepository.java
+++ backend/src/main/java/com/kett/TicketSystem/project/repository/ProjectRepository.java
@@ -1,11 +1,11 @@
 package com.kett.TicketSystem.project.repository;
 
 import com.kett.TicketSystem.project.domain.Project;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, UUID> {
+public interface ProjectRepository extends CrudRepository<Project, UUID> {
 }
