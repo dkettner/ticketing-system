9f904b7e723938f6ed30f463572f8567148a7ca1
dkettner <kettner_dennis@web.de>
2022-10-23 13:56:24 +0200

    feat(b-ticket): add ticketRepository to ProjectService

--- backend/src/main/java/com/kett/TicketSystem/project/application/ProjectService.java
+++ backend/src/main/java/com/kett/TicketSystem/project/application/ProjectService.java
@@ -3,6 +3,7 @@ package com.kett.TicketSystem.project.application;
 import com.kett.TicketSystem.project.domain.Project;
 import com.kett.TicketSystem.project.domain.exceptions.NoProjectFoundException;
 import com.kett.TicketSystem.project.repository.ProjectRepository;
+import com.kett.TicketSystem.project.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +12,12 @@ import java.util.UUID;
 @Service
 public class ProjectService {
     private final ProjectRepository projectRepository;
+    private final TicketRepository ticketRepository;
 
     @Autowired
-    public ProjectService(ProjectRepository projectRepository) {
+    public ProjectService(ProjectRepository projectRepository, TicketRepository ticketRepository) {
         this.projectRepository = projectRepository;
+        this.ticketRepository = ticketRepository;
     }
 
     public Project getProjectById(UUID id) {
