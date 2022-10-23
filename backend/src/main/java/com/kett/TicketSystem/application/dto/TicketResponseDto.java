b52146b593919c495d56821f3ba6a3ab52daa347
dkettner <kettner_dennis@web.de>
2022-10-23 15:45:13 +0200

    feat(b-ticket): add TicketResponseDto

---
+++ backend/src/main/java/com/kett/TicketSystem/application/dto/TicketResponseDto.java
@@ -0,0 +1,24 @@
+package com.kett.TicketSystem.application.dto;
+
+import com.kett.TicketSystem.project.domain.TicketStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketResponseDto {
+    private UUID ticketNumber;
+    private String title;
+    private String description;
+    private LocalDateTime creationTime;
+    private LocalDateTime dueTime;
+    private TicketStatus ticketStatus;
+    private UUID creatorId;
+    private List<UUID> assigneeIds = new ArrayList<>();
+}
