16e6142b6cebab40a4fab52c593c840dca8f6f45
dkettner <kettner_dennis@web.de>
2022-10-23 13:41:03 +0200

    feat(b-ticket): add exception handlers for custom exceptions to controller

--- backend/src/main/java/com/kett/TicketSystem/application/ProjectController.java
+++ backend/src/main/java/com/kett/TicketSystem/application/ProjectController.java
@@ -2,8 +2,10 @@ package com.kett.TicketSystem.application;
 
 import com.kett.TicketSystem.application.dto.ProjectResponseDto;
 
+import com.kett.TicketSystem.project.domain.exceptions.NoTicketFoundException;
 import com.kett.TicketSystem.project.domain.exceptions.ProjectException;
 import com.kett.TicketSystem.project.domain.exceptions.NoProjectFoundException;
+import com.kett.TicketSystem.project.domain.exceptions.TicketException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,12 +23,15 @@ public class ProjectController {
         this.ticketSystemService = ticketSystemService;
     }
 
+    // rest endpoints
+
     @GetMapping("/{id}")
     public ResponseEntity<ProjectResponseDto> getProjectDtoById(@PathVariable UUID id) {
         ProjectResponseDto projectResponseDto = ticketSystemService.fetchProjectResponseDtoById(id);
         return new ResponseEntity<>(projectResponseDto, HttpStatus.OK);
     }
 
+    // exception handlers
 
     @ExceptionHandler(ProjectException.class)
     public ResponseEntity<String> handleProjectException(ProjectException projectException) {
@@ -36,5 +41,15 @@ public class ProjectController {
     @ExceptionHandler(NoProjectFoundException.class)
     public ResponseEntity<String> handleNoProjectFoundException(NoProjectFoundException noProjectFoundException) {
         return new ResponseEntity<>(noProjectFoundException.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(TicketException.class)
+    public ResponseEntity<String> handleTicketException(TicketException ticketException) {
+        return new ResponseEntity<>(ticketException.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoTicketFoundException.class)
+    public ResponseEntity<String> handleNoTicketFoundException(NoTicketFoundException noTicketFoundException) {
+        return new ResponseEntity<>(noTicketFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
