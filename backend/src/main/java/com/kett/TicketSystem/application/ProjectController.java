631f7cfb321a77a275be06203d588b1b2e3bd7ad
dkettner <kettner_dennis@web.de>
2022-10-24 11:34:20 +0200

    fix(b-project): make ProjectController transactional
    delete of project failed because no transaction was created

--- backend/src/main/java/com/kett/TicketSystem/application/ProjectController.java
+++ backend/src/main/java/com/kett/TicketSystem/application/ProjectController.java
@@ -8,6 +8,7 @@ import com.kett.TicketSystem.project.domain.exceptions.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -16,6 +17,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@Transactional
 @RequestMapping("/projects")
 public class ProjectController {
     private final TicketSystemService ticketSystemService;
