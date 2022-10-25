408c76810ec221dbb2204be24978637aa71cd739
dkettner <kettner_dennis@web.de>
2022-10-25 11:56:49 +0200

    feat(dto): add TicketPatchDto

---
+++ backend/src/main/java/com/kett/TicketSystem/application/dto/TicketPatchDto.java
@@ -0,0 +1,22 @@
+package com.kett.TicketSystem.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TicketPatchDto {
+    private String title;
+    private String description;
+    private LocalDateTime dueTime;
+    private List<UUID> assigneeIds = new ArrayList<>();
+}
