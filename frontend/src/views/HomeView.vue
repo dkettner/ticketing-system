d5a4583f8a328e9b2b6af1bcd47abc5c69daf09c
dkettner <kettner_dennis@web.de>
2022-10-25 19:03:10 +0200

    refactor: remove default components from frontend

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <h1>Home</h1>
+  </div>
 </template>
