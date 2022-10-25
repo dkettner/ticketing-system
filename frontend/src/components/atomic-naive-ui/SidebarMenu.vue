6fb0176f90ed4090a21d29f82995031f74ece584
dkettner <kettner_dennis@web.de>
2022-10-26 00:15:45 +0200

    feat(f-dashboard): add collapsable sidebar for projects

---
+++ frontend/src/components/atomic-naive-ui/SidebarMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <n-space vertical>
+    <n-layout has-sider>
+      <n-layout-sider
+        bordered
+        collapse-mode="width"
+        :collapsed-width="64"
+        :width="240"
+        show-trigger
+        :collapsed="collapsed"
+        @collapse="collapsed = true"
+        @expand="collapsed = false"
+      >
+        <n-menu
+          :collapsed="collapsed"
+          :collapsed-width="64"
+          :options="menuOptions"
+        />
+      </n-layout-sider>
+      <n-layout>
+        <span>Content</span>
+      </n-layout>
+    </n-layout>
+  </n-space>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { NSpace, NLayout, NLayoutSider, NMenu} from "naive-ui";
+
+const collapsed = ref(false);
+const menuOptions = [
+  {
+    label: "Project 0",
+    key: "project0"
+  },
+  {
+    label: "Project 1",
+    key: "project1"
+  },
+  {
+    label: "Project 2",
+    key: "project2",
+  }
+];
+</script>
--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,6 +1,8 @@
 <script setup>
+  import SidebarMenuVue from '../components/atomic-naive-ui/SidebarMenu.vue';
 </script>
 
 <template>
   <h1>Dashboard</h1>
+  <SidebarMenuVue />
 </template>
