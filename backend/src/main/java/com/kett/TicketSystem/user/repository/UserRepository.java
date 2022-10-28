187857437b6d9d5ad1f1a85e3d8281139feead54
dkettner <kettner_dennis@web.de>
2022-10-28 14:46:30 +0200

    feat(b-user): add UserRepository

---
+++ backend/src/main/java/com/kett/TicketSystem/user/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.kett.TicketSystem.user.repository;
+
+import com.kett.TicketSystem.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
