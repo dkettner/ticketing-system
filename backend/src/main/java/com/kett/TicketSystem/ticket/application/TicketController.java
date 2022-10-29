2e3f0bb2a35f6125342e94b45677386848822e39
dkettner <kettner_dennis@web.de>
2022-10-29 10:40:32 +0200

    feat(b-ticket): add exception handlers to TicketController

--- backend/src/main/java/com/kett/TicketSystem/ticket/application/TicketController.java
+++ backend/src/main/java/com/kett/TicketSystem/ticket/application/TicketController.java
@@ -1,9 +1,14 @@
 package com.kett.TicketSystem.ticket.application;
 
 import com.kett.TicketSystem.application.TicketSystemService;
+import com.kett.TicketSystem.ticket.domain.exceptions.NoTicketFoundException;
+import com.kett.TicketSystem.ticket.domain.exceptions.TicketException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,18 @@ public class TicketController {
     @Autowired
     public TicketController(TicketSystemService ticketSystemService) {
         this.ticketSystemService = ticketSystemService;
+    }
+
+
+    // exception handlers
+
+    @ExceptionHandler(TicketException.class)
+    public ResponseEntity<String> handleTicketException(TicketException ticketException) {
+        return new ResponseEntity<>(ticketException.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoTicketFoundException.class)
+    public ResponseEntity<String> handleNoTicketFoundException(NoTicketFoundException noTicketFoundException) {
+        return new ResponseEntity<>(noTicketFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
