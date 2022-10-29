f10afb94db7f6540bfc62b0ba083cd9a0e7338ab
dkettner <kettner_dennis@web.de>
2022-10-29 09:59:20 +0200

    feat(b-ticket): add TicketService

---
+++ backend/src/main/java/com/kett/TicketSystem/ticket/application/dto/TicketService.java
@@ -0,0 +1,15 @@
+package com.kett.TicketSystem.ticket.application.dto;
+
+import com.kett.TicketSystem.ticket.repository.TicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TicketService {
+    private final TicketRepository ticketRepository;
+
+    @Autowired
+    public TicketService(TicketRepository ticketRepository) {
+        this.ticketRepository = ticketRepository;
+    }
+}
