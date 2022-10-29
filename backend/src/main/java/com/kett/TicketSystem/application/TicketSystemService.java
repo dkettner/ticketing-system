800690aca8a1e00c2770bfe86f5cc297f9d7d15d
dkettner <kettner_dennis@web.de>
2022-10-29 10:46:41 +0200

    refactor(b-project): remove old ticket methods that belonged to ProjectService

--- backend/src/main/java/com/kett/TicketSystem/application/TicketSystemService.java
+++ backend/src/main/java/com/kett/TicketSystem/application/TicketSystemService.java
@@ -37,16 +37,6 @@ public class TicketSystemService {
         return dtoMapper.mapProjectToProjectResponseDto(project);
     }
 
-    public List<TicketResponseDto> fetchTicketsByProjectId(UUID id) {
-        List<Ticket> tickets = projectService.getTicketsByProjectId(id);
-        return dtoMapper.mapTicketListToTicketResponseDtoList(tickets);
-    }
-
-    public TicketResponseDto fetchTicketByProjectIdAndTicketNumber(UUID id, UUID ticketNumber) {
-        Ticket ticket = projectService.getTicketByProjectIdAndTicketNumber(id, ticketNumber);
-        return dtoMapper.mapTicketToTicketResponseDto(ticket);
-    }
-
     public ProjectResponseDto addProject(ProjectPostDto projectPostDto) {
         Project project = projectService.addProject(
                 dtoMapper.mapProjectPostDtoToProject(projectPostDto)
@@ -64,28 +54,6 @@ public class TicketSystemService {
                 id, projectPatchDto.getName(),
                 projectPatchDto.getDescription(),
                 projectPatchDto.getMemberIds());
-    }
-
-    public TicketResponseDto addTicketToProject(UUID id, TicketPostDto ticketPostDto) {
-        Ticket ticket = projectService.addTicketToProject(id, dtoMapper.mapTicketPostDtoToTicket(ticketPostDto));
-        return dtoMapper.mapTicketToTicketResponseDto(ticket);
-    }
-
-    public void deleteTicketByProjectIdAndTicketNumber(UUID id, UUID ticketNumber) {
-        projectService.deleteTicketByProjectIdAndTicketNumber(id, ticketNumber);
-    }
-
-    // TODO: clean this up
-    public void patchTicket(UUID id, UUID ticketNumber, TicketPatchDto ticketPatchDto) {
-        projectService.patchTicket(
-                id,
-                ticketNumber,
-                ticketPatchDto.getTitle(),
-                ticketPatchDto.getDescription(),
-                ticketPatchDto.getDueTime(),
-                ticketPatchDto.getTicketStatus(),
-                ticketPatchDto.getAssigneeIds()
-        );
     }
 
     public List<ProjectResponseDto> fetchAllProjects() {
