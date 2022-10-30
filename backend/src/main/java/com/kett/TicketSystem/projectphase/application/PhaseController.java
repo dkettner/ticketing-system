d07386045921ec7545033c6a645194d349a91104
dkettner <kettner_dennis@web.de>
2022-10-30 22:50:53 +0100

    feat(b-phase): add exception handlers to PhaseController

--- backend/src/main/java/com/kett/TicketSystem/projectphase/application/PhaseController.java
+++ backend/src/main/java/com/kett/TicketSystem/projectphase/application/PhaseController.java
@@ -1,9 +1,14 @@
 package com.kett.TicketSystem.projectphase.application;
 
 import com.kett.TicketSystem.application.TicketSystemService;
+import com.kett.TicketSystem.projectphase.domain.exceptions.NoPhaseFoundException;
+import com.kett.TicketSystem.projectphase.domain.exceptions.PhaseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +22,17 @@ public class PhaseController {
     @Autowired
     public PhaseController(TicketSystemService ticketSystemService) {
         this.ticketSystemService = ticketSystemService;
+    }
+
+
+    // exception handlers
+    @ExceptionHandler(PhaseException.class)
+    public ResponseEntity<String> handlePhaseException(PhaseException phaseException) {
+        return new ResponseEntity<>(phaseException.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NoPhaseFoundException.class)
+    public ResponseEntity<String> handleNoPhaseFoundException(NoPhaseFoundException noPhaseFoundException) {
+        return new ResponseEntity<>(noPhaseFoundException.getMessage(), HttpStatus.NOT_FOUND);
     }
 }
