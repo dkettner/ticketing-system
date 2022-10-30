b9343cf6a00a194fb9f1a3c16c9aa05ac0563dd5
dkettner <kettner_dennis@web.de>
2022-10-30 23:02:01 +0100

    add(b-phase): add PhaseResponseDto

---
+++ backend/src/main/java/com/kett/TicketSystem/phase/application/dto/PhaseResponseDto.java
@@ -0,0 +1,20 @@
+package com.kett.TicketSystem.phase.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PhaseResponseDto {
+    private UUID id;
+    private UUID projectId;
+    private String name;
+    private UUID previousPhase;
+    private UUID nextPhase;
+}
