249931ede2f955b1867a986715e1c29b3b88d154
dkettner <kettner_dennis@web.de>
2022-10-30 22:45:38 +0100

    fix(b-phase): add PhaseService to application layer

--- backend/src/main/java/com/kett/TicketSystem/application/TicketSystemService.java
+++ backend/src/main/java/com/kett/TicketSystem/application/TicketSystemService.java
@@ -3,6 +3,7 @@ package com.kett.TicketSystem.application;
 import com.kett.TicketSystem.project.application.ProjectService;
 import com.kett.TicketSystem.project.application.dto.*;
 import com.kett.TicketSystem.project.domain.Project;
+import com.kett.TicketSystem.projectphase.application.PhaseService;
 import com.kett.TicketSystem.ticket.application.TicketService;
 import com.kett.TicketSystem.ticket.application.dto.TicketPatchDto;
 import com.kett.TicketSystem.ticket.application.dto.TicketPostDto;
@@ -20,13 +21,16 @@ import java.util.UUID;
 @Service
 public class TicketSystemService {
     private final ProjectService projectService;
+    private final PhaseService phaseService;
     private final TicketService ticketService;
     private final UserService userService;
     private final DtoMapper dtoMapper;
 
     @Autowired
-    public TicketSystemService (ProjectService projectService, TicketService ticketService, UserService userService) {
+    public TicketSystemService (ProjectService projectService, PhaseService phaseService,
+                                TicketService ticketService, UserService userService) {
         this.projectService = projectService;
+        this.phaseService = phaseService;
         this.ticketService = ticketService;
         this.userService = userService;
         this.dtoMapper = new DtoMapper();
