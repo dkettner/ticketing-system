377951a18dae5d14f1a18bb1ecfdaf7f7c3b27e0
dkettner <kettner_dennis@web.de>
2022-10-30 22:43:46 +0100

    feat(b-phase): add PhaseController

---
+++ backend/src/main/java/com/kett/TicketSystem/projectphase/application/PhaseController.java
@@ -0,0 +1,21 @@
+package com.kett.TicketSystem.projectphase.application;
+
+import com.kett.TicketSystem.application.TicketSystemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Transactional
+@CrossOrigin(origins = {"http://127.0.0.1:5173"})
+@RequestMapping("/phases")
+public class PhaseController {
+    private final TicketSystemService ticketSystemService;
+
+    @Autowired
+    public PhaseController(TicketSystemService ticketSystemService) {
+        this.ticketSystemService = ticketSystemService;
+    }
+}
