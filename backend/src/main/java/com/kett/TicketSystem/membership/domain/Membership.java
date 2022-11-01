f09ceaee964fc7ec031b6af680c3a2cebb8dc59d
dkettner <kettner_dennis@web.de>
2022-11-01 14:22:33 +0100

    refactor(b-membership): add Membership entity

---
+++ backend/src/main/java/com/kett/TicketSystem/membership/domain/Membership.java
@@ -0,0 +1,36 @@
+package com.kett.TicketSystem.membership.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Membership {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter(AccessLevel.PROTECTED)
+    @Column(length = 16)
+    private UUID id;
+
+    @Getter
+    @Setter(AccessLevel.PROTECTED)
+    @Column(length = 16)
+    private UUID projectId;
+
+    @Getter
+    @Setter(AccessLevel.PROTECTED)
+    @Column(length = 16)
+    private UUID UserId;
+
+    @Getter
+    @Setter
+    private String role;
+
+    @Getter
+    @Setter
+    private String state;
+}
