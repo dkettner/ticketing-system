cfaf78d8a80e3b8f99fd8848a8aacacdc84f9589
dkettner <kettner_dennis@web.de>
2022-11-08 15:14:20 +0100

    feat(b-security): add memberships as authorities to jwt

--- backend/src/main/java/com/kett/TicketSystem/membership/repository/MembershipRepository.java
+++ backend/src/main/java/com/kett/TicketSystem/membership/repository/MembershipRepository.java
@@ -1,6 +1,7 @@
 package com.kett.TicketSystem.membership.repository;
 
 import com.kett.TicketSystem.membership.domain.Membership;
+import com.kett.TicketSystem.membership.domain.State;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.UUID;
 public interface MembershipRepository extends JpaRepository<Membership, UUID> {
     List<Membership> findByUserId(UUID userId);
     List<Membership> findByProjectId(UUID projectId);
+    List<Membership> findByUserIdAndStateEquals(UUID projectId, State state);
     Long removeById(UUID id);
 
     Boolean existsByUserIdAndProjectId(UUID userId, UUID projectId);
