77c5a256c8004317f46316c1810fa50ca177a3b8
dkettner <kettner_dennis@web.de>
2022-11-17 21:06:59 +0100

    feat(f-login): message on sign up, redirecting to sing in

--- frontend/src/views/SignInView.vue
+++ frontend/src/views/SignInView.vue
@@ -1,11 +1,13 @@
 <script setup>
-  import { NSpace } from 'naive-ui';
+  import { NSpace, NNotificationProvider } from 'naive-ui';
   import SignInForm from '../components/atomic-naive-ui/SignInForm.vue';
 </script>
 
 <template>
   <n-space justify="center">
     <img src="../assets/ticketsystem-logo.png">
-    <SignInForm />
+    <n-notification-provider>
+      <SignInForm />
+    </n-notification-provider>
   </n-space>
 </template>
