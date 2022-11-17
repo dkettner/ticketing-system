1e2261ca96cd271d66671623dbe1b0cf8d83f771
dkettner <kettner_dennis@web.de>
2022-11-17 14:04:37 +0100

    feat(f-state): add pinia for state management

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import App from './App.vue';
+import router from './router';
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
+app.use(router);
+app.use(createPinia());
 
-app.mount('#app')
+app.mount('#app');
