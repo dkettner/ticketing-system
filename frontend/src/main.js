6fb3bd971777ac67fc8718999fc2ea3db8796c4f
dkettner <kettner_dennis@web.de>
2022-11-18 20:48:25 +0100

    fix(f-requests): mount axios config to App

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
+import './axios';
 
 const app = createApp(App);
 
