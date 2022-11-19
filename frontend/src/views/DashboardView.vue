9e014078161f558add6805a695be8de8925fb747
dkettner <kettner_dennis@web.de>
2022-11-19 13:47:50 +0100

    feat(f-user): add simple greeter

--- frontend/src/views/DashboardView.vue
+++ frontend/src/views/DashboardView.vue
@@ -1,10 +1,21 @@
 <script setup>
   import SidebarMenuVue from '../components/atomic-naive-ui/SidebarMenu.vue';
   import { NNotificationProvider } from 'naive-ui';
+  import { storeToRefs } from 'pinia';
+  import { useUserStore } from '../stores/user.js'
+import { onMounted } from 'vue';
+  
+  const userStore = useUserStore()
+  const { user } = storeToRefs(userStore);
+
+  onMounted( async () => {
+    userStore.updateUserByEmail()
+  });
 </script>
 
 <template>
   <h1 style="padding-left: 25px;">Dashboard</h1>
+  <h1>Hello {{user.name}}!</h1>
   <n-notification-provider>
     <SidebarMenuVue />
   </n-notification-provider>
