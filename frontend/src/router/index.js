74772bacb16f70bb7d32a392479a704fe5ad4ba3
dkettner <kettner_dennis@web.de>
2022-11-22 01:45:22 +0100

    feat(f-routing): add global navigation guard

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,31 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import SignInView from '../views/SignInView.vue'
+import { useSessionStore } from '../stores/session.js'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    {
-      path: '/',
-      redirect: () => {
-        return '/signin';
-      }
-      
-    },
     {
       path: '/signin',
       name: 'signin',
       component: SignInView
     },
     {
+      path: '/',
+      redirect: to => {
+        return '/dashboard';
+      }
+    },
+    {
       path: '/dashboard',
       name: 'dashboard',
-      component: () => import('../views/DashboardView.vue')
+      component: () => import('../views/DashboardView.vue'),
     },
     {
       path: '/:pathMatch(.*)',
       component: () => import('../views/NotFoundView.vue')
     }
   ]
-})
+});
+
+router.beforeEach( (to, from, next) => {
+  const sessionStore = useSessionStore();
+
+  if (to.name === 'signin' && sessionStore.isLoggedIn()) {
+    next({ name: 'dashboard' });
+  } else if (to.name !== 'signin' && !(sessionStore.isLoggedIn())) {
+    next({ name: 'signin' });
+  } else {
+    next();
+  }
+});
 
 export default router
