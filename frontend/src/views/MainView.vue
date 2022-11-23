cf02695de321e049e869b822f76c61093feccaf0
dkettner <kettner_dennis@web.de>
2022-11-23 02:24:08 +0100

    feat(f-state): initialize projects and memberships states on login

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -3,17 +3,23 @@
   import { storeToRefs } from 'pinia';
   import { useUserStore } from '../stores/user.js';
   import { useSessionStore } from '../stores/session';
+  import { useProjectStore } from '../stores/project';
+  import { useMembershipStore } from '../stores/membership';
   import { NSpace, NLayout, NLayoutHeader} from 'naive-ui';
 
   import SidebarMenuVue from '../components/atomic-naive-ui/SidebarMenu.vue';
 
   const sessionStore = useSessionStore();
+  const membershipStore = useMembershipStore();
+  const projectStore = useProjectStore();
   const userStore = useUserStore();
   const { user } = storeToRefs(userStore);
 
   onMounted( async () => {
     userStore.setEmail(sessionStore.email);
     await userStore.updateUserByEmail();
+    await membershipStore.updateMembershipsByEmail();
+    await projectStore.updateProjectsByAcceptedMemberships();
   });
 </script>
 
