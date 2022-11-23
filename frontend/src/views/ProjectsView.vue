dd175c227c64c2581da0b6a2c852e060c0ca3365
dkettner <kettner_dennis@web.de>
2022-11-23 02:24:47 +0100

    feat(f-projects): display all projects in ProjectsView

--- frontend/src/views/ProjectsView.vue
+++ frontend/src/views/ProjectsView.vue
@@ -1,8 +1,23 @@
 <script setup>
+  import { useProjectStore } from '../stores/project';
+  import { useMembershipStore } from '../stores/membership';
+  import { storeToRefs } from 'pinia';
+  import { NCard } from 'naive-ui';
+
+  const membershipStore = useMembershipStore();
+  const memberships = storeToRefs(membershipStore);
+  const projectStore = useProjectStore();
+  const { projects } = storeToRefs(projectStore);
 </script>
 
 <template>
   <div>
     <h1 style="padding-left: 25px;">Imagine all your projects right here.</h1>
+    <div v-for="project in projects" style="padding-left: 25px;">
+      <n-card :title="project.name" embedded hoverable style="max-width: 400px; ">
+        {{project.description}}
+        <p style="font-style: italic;">created at: {{project.creationTime}}</p>
+      </n-card>
+    </div>
   </div>
 </template>
