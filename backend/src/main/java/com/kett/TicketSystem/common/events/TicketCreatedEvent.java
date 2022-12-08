b988376f0144378d1381551dda0bf78f78cc59b4
dkettner <kettner_dennis@web.de>
2022-12-08 22:34:04 +0100

    feat(b-ticket): add TicketCreatedEvent

---
+++ backend/src/main/java/com/kett/TicketSystem/common/events/TicketCreatedEvent.java
@@ -0,0 +1,23 @@
+package com.kett.TicketSystem.common.events;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class TicketCreatedEvent {
+    private final UUID id;
+    private final LocalDateTime timeStamp;
+    private final UUID ticketId;
+    private final UUID projectId;
+    private final UUID userId;
+
+    public TicketCreatedEvent(UUID ticketId, UUID projectId, UUID userId) {
+        this.id = UUID.randomUUID();
+        this.timeStamp = LocalDateTime.now();
+        this.ticketId = ticketId;
+        this.projectId = projectId;
+        this.userId = userId;
+    }
+}
