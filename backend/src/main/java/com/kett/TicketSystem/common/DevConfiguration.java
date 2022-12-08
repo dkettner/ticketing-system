5999867c733a19646adad0534ebcd007b5c276c5
dkettner <kettner_dennis@web.de>
2022-12-09 00:16:14 +0100

    feat(b-security): add profile for cors configuration

---
+++ backend/src/main/java/com/kett/TicketSystem/common/DevConfiguration.java
@@ -0,0 +1,15 @@
+package com.kett.TicketSystem.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@Profile("development")
+public class DevConfiguration implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS").allowedOrigins("http://localhost:5173");
+    }
+}
