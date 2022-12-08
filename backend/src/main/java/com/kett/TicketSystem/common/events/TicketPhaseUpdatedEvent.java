09ab7ad235fdbf2e41af0ae326fd2dc62b57a1ce
dkettner <kettner_dennis@web.de>
2022-12-08 22:38:33 +0100

    feat(b-ticket): add TicketPhaseUpdatedEvent

---
+++ backend/src/main/java/com/kett/TicketSystem/common/events/TicketPhaseUpdatedEvent.java
@@ -0,0 +1,27 @@
+package com.kett.TicketSystem.common.events;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+public class TicketPhaseUpdatedEvent{
+    private final UUID id;
+    private final LocalDateTime timeStamp;
+    private final UUID ticketId;
+    private final UUID oldPhaseId;
+    private final UUID newPhaseId;
+    private final UUID projectId;
+    private final UUID userId;
+
+    public TicketPhaseUpdatedEvent(UUID ticketId, UUID oldPhaseId, UUID projectId, UUID userId, UUID newPhaseId) {
+        this.id = UUID.randomUUID();
+        this.timeStamp = LocalDateTime.now();
+        this.ticketId = ticketId;
+        this.projectId = projectId;
+        this.userId = userId;
+        this.oldPhaseId = oldPhaseId;
+        this.newPhaseId = newPhaseId;
+    }
+}
