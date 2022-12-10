8d79b113c36598a3d780bde0b90b8320cdd69011
dkettner <kettner_dennis@web.de>
2022-12-10 16:26:18 +0100

    refactor(b-profiles): move development profile to profiles directory

--- backend/src/main/java/com/kett/TicketSystem/common/profiles/DevConfiguration.java
+++ backend/src/main/java/com/kett/TicketSystem/common/profiles/DevConfiguration.java
@@ -1,4 +1,4 @@
-package com.kett.TicketSystem.common;
+package com.kett.TicketSystem.common.profiles;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
