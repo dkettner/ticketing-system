1983a5f7f5b88bc451121c82cdc8674d83907809
dkettner <kettner_dennis@web.de>
2022-12-16 00:52:53 +0100

    refactor(b-ticket): remove userId from TicketPhaseUpdatedEvent

--- backend/src/main/java/com/kett/TicketSystem/ticket/domain/events/TicketPhaseUpdatedEvent.java
+++ backend/src/main/java/com/kett/TicketSystem/ticket/domain/events/TicketPhaseUpdatedEvent.java
@@ -10,17 +10,15 @@ public class TicketPhaseUpdatedEvent{
     private final UUID id;
     private final LocalDateTime timeStamp;
     private final UUID ticketId;
+    private final UUID projectId;
     private final UUID oldPhaseId;
     private final UUID newPhaseId;
-    private final UUID projectId;
-    private final UUID userId;
 
-    public TicketPhaseUpdatedEvent(UUID ticketId, UUID oldPhaseId, UUID projectId, UUID userId, UUID newPhaseId) {
+    public TicketPhaseUpdatedEvent(UUID ticketId, UUID projectId, UUID oldPhaseId, UUID newPhaseId) {
         this.id = UUID.randomUUID();
         this.timeStamp = LocalDateTime.now();
         this.ticketId = ticketId;
         this.projectId = projectId;
-        this.userId = userId;
         this.oldPhaseId = oldPhaseId;
         this.newPhaseId = newPhaseId;
     }
