5107abc254ff584b171ae845b4829752fe3e6324
dkettner <kettner_dennis@web.de>
2023-02-03 22:38:04 +0100

    feat(b-notification): add findByRecipientId query to repository

--- backend/src/main/java/com/kett/TicketSystem/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/kett/TicketSystem/notification/repository/NotificationRepository.java
@@ -4,8 +4,10 @@ import com.kett.TicketSystem.notification.domain.Notification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
+    List<Notification> findByRecipientId(UUID recipientId);
 }
