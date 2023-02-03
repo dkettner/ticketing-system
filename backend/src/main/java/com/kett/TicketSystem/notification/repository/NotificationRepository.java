f23b0f697b2c0ff85e7b82b6922ddc980ce540d2
dkettner <kettner_dennis@web.de>
2023-02-03 22:41:23 +0100

    feat(b-notification): add findByRecipientIdAndIsReadFalse query to repository

--- backend/src/main/java/com/kett/TicketSystem/notification/repository/NotificationRepository.java
+++ backend/src/main/java/com/kett/TicketSystem/notification/repository/NotificationRepository.java
@@ -10,4 +10,5 @@ import java.util.UUID;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, UUID> {
     List<Notification> findByRecipientId(UUID recipientId);
+    List<Notification> findByRecipientIdAndIsReadFalse(UUID recipientId);
 }
