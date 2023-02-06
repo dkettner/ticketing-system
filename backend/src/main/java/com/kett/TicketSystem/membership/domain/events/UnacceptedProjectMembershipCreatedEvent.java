2c32ece10b3cf66a8969b359fcf0c551946aebb7
dkettner <kettner_dennis@web.de>
2023-02-06 21:58:38 +0100

    feat(b-membership): add UnacceptedProjectMembershipCreatedEvent

---
+++ backend/src/main/java/com/kett/TicketSystem/membership/domain/events/UnacceptedProjectMembershipCreatedEvent.java
@@ -0,0 +1,18 @@
+package com.kett.TicketSystem.membership.domain.events;
+
+import com.kett.TicketSystem.common.DomainEvent;
+import lombok.Getter;
+
+import java.util.UUID;
+
+@Getter
+public class UnacceptedProjectMembershipCreatedEvent extends DomainEvent {
+    private final UUID inviteeId;
+    private final UUID projectId;
+
+    public UnacceptedProjectMembershipCreatedEvent(UUID inviteeId, UUID projectId) {
+        super();
+        this.inviteeId = inviteeId;
+        this.projectId = projectId;
+    }
+}
