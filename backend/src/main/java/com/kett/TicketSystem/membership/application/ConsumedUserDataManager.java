931d555c6cbe41daa42fa1c5be485eccf933257c
dkettner <kettner_dennis@web.de>
2023-02-28 15:26:34 +0100

    feat(b-membership): add ConsumedUserDataManager
    implements IConsumedDataManager

---
+++ backend/src/main/java/com/kett/TicketSystem/membership/application/ConsumedUserDataManager.java
@@ -0,0 +1,51 @@
+package com.kett.TicketSystem.membership.application;
+
+import com.kett.TicketSystem.common.IConsumedDataManager;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public class ConsumedUserDataManager implements IConsumedDataManager<UUID> {
+    List<UUID> existingUsers = new ArrayList<>(); // MembershipService only cares about the IDs of the users
+
+    @Override
+    public Boolean add(UUID date) {
+        if (this.exists(date)) {
+            return false;
+        }
+
+        existingUsers.add(date);
+        return true;
+    }
+
+    @Override
+    public Boolean overwrite(UUID date) {
+        if (this.exists(date)) {
+            existingUsers.remove(date);
+            existingUsers.add(date);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public Boolean remove(UUID id) {
+        return existingUsers.remove(id);
+    }
+
+    @Override
+    public Optional<UUID> get(UUID id) {  // in this case redundant
+        return existingUsers
+                .stream()
+                .filter(projectId -> projectId.equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public Boolean exists(UUID id) {
+        return existingUsers.contains(id);
+    }
+}
