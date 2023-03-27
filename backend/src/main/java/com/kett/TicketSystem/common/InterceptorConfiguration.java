1efb0c7112833fe2f77e29ae781655509d70cee4
dkettner <kettner_dennis@web.de>
2023-03-27 16:57:17 +0200

    feat(b-logging): add LoggingConfiguration

---
+++ backend/src/main/java/com/kett/TicketSystem/common/InterceptorConfiguration.java
@@ -0,0 +1,21 @@
+package com.kett.TicketSystem.common;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class InterceptorConfiguration implements WebMvcConfigurer {
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Autowired
+    public InterceptorConfiguration(LoggingInterceptor loggingInterceptor) {
+        this.loggingInterceptor = loggingInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
