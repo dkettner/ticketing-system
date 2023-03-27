10f3d99fb53c9684c874dbedcf849d63934a5e78
dkettner <kettner_dennis@web.de>
2023-03-27 16:33:38 +0200

    feat(b-logging): add LoggingInterceptor

---
+++ backend/src/main/java/com/kett/TicketSystem/common/LoggingInterceptor.java
@@ -0,0 +1,36 @@
+package com.kett.TicketSystem.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.UUID;
+
+public class LoggingInterceptor implements HandlerInterceptor {
+    private final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        MDC.put("transactionId", UUID.randomUUID().toString());
+
+        Map<String, Object> inputMap = new ObjectMapper().readValue(request.getInputStream(), Map.class);
+        logger.trace(request.getMethod() + " " + request.getRequestURI());
+        logger.info("Full request: " + inputMap);
+
+        return true;
+    }
+
+    @Override
+    public void postHandle(
+            HttpServletRequest request, HttpServletResponse response, Object handler,
+                            @Nullable ModelAndView modelAndView) throws Exception {
+        MDC.remove("transactionId");
+    }
+}
