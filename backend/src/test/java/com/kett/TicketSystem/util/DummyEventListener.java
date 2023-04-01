91d78887f41dfbb6d0c5c3a46379960d2b1ce0d4
dkettner <kettner_dennis@web.de>
2023-04-01 15:18:16 +0200

    feat(b-tests): add DummyEventListener

---
+++ backend/src/test/java/com/kett/TicketSystem/util/DummyEventListener.java
@@ -0,0 +1,39 @@
+package com.kett.TicketSystem.util;
+
+import com.kett.TicketSystem.user.domain.events.UserCreatedEvent;
+import com.kett.TicketSystem.user.domain.events.UserDeletedEvent;
+import org.springframework.context.event.EventListener;
+
+import java.util.Optional;
+import java.util.Stack;
+
+public class DummyEventListener {
+    Stack<UserCreatedEvent> userCreatedEvents = new Stack<>();
+    Stack<UserDeletedEvent> userDeletedEvents = new Stack<>();
+
+    public Optional<UserCreatedEvent> getLatestUserCreatedEvent() {
+        Optional<UserCreatedEvent> event = Optional.empty();
+        if (!userCreatedEvents.isEmpty()) {
+            event = Optional.of(userCreatedEvents.pop());
+        }
+        return event;
+    }
+
+    public Optional<UserDeletedEvent> getLatestUserDeletedEvent() {
+        Optional<UserDeletedEvent> event = Optional.empty();
+        if (!userDeletedEvents.isEmpty()) {
+            event = Optional.of(userDeletedEvents.pop());
+        }
+        return event;
+    }
+
+
+    @EventListener
+    public void handleUserCreatedEvent(UserCreatedEvent userCreatedEvent) {
+        userCreatedEvents.push(userCreatedEvent);
+    }
+    @EventListener
+    public void handleUserDeletedEvent(UserDeletedEvent userDeletedEvent) {
+        userDeletedEvents.push(userDeletedEvent);
+    }
+}
