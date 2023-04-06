5da2f3953f5474d78a402e07b98e071ba1facfe8
dkettner <kettner_dennis@web.de>
2023-04-06 14:06:24 +0200

    feat(b-test): handle UserPatchedEvents in DummyEventListener

--- backend/src/test/java/com/kett/TicketSystem/util/DummyEventListener.java
+++ backend/src/test/java/com/kett/TicketSystem/util/DummyEventListener.java
@@ -2,6 +2,7 @@ package com.kett.TicketSystem.util;
 
 import com.kett.TicketSystem.user.domain.events.UserCreatedEvent;
 import com.kett.TicketSystem.user.domain.events.UserDeletedEvent;
+import com.kett.TicketSystem.user.domain.events.UserPatchedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +13,7 @@ import java.util.Stack;
 public class DummyEventListener {
     Stack<UserCreatedEvent> userCreatedEvents = new Stack<>();
     Stack<UserDeletedEvent> userDeletedEvents = new Stack<>();
+    Stack<UserPatchedEvent> userPatchedEvents = new Stack<>();
 
     public Optional<UserCreatedEvent> getLatestUserCreatedEvent() {
         Optional<UserCreatedEvent> event = Optional.empty();
@@ -34,13 +36,26 @@ public class DummyEventListener {
         userDeletedEvents.clear();
     }
 
+    public Optional<UserPatchedEvent> getLatestUserPatchedEvent() {
+        Optional<UserPatchedEvent> event = Optional.empty();
+        if (!userPatchedEvents.isEmpty()) {
+            event = Optional.of(userPatchedEvents.pop());
+        }
+        return event;
+    }
 
     @EventListener
     public void handleUserCreatedEvent(UserCreatedEvent userCreatedEvent) {
         userCreatedEvents.push(userCreatedEvent);
     }
+
     @EventListener
     public void handleUserDeletedEvent(UserDeletedEvent userDeletedEvent) {
         userDeletedEvents.push(userDeletedEvent);
+    }
+
+    @EventListener
+    public void handleUserPatchedEvent(UserPatchedEvent userPatchedEvent) {
+        userPatchedEvents.push(userPatchedEvent);
     }
 }
