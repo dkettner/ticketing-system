58d54dc9654d634e6f9e8290cd8938d7e9b64f99
dkettner <kettner_dennis@web.de>
2023-04-08 12:23:07 +0200

    feat(b-test): add post project test

--- backend/src/test/java/com/kett/TicketSystem/project/application/ProjectControllerTests.java
+++ backend/src/test/java/com/kett/TicketSystem/project/application/ProjectControllerTests.java
@@ -4,10 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
 import com.kett.TicketSystem.authentication.dto.AuthenticationPostDto;
 import com.kett.TicketSystem.common.domainprimitives.EmailAddress;
+import com.kett.TicketSystem.membership.domain.events.LastProjectMemberDeletedEvent;
+import com.kett.TicketSystem.project.application.dto.ProjectPostDto;
 import com.kett.TicketSystem.project.domain.Project;
 import com.kett.TicketSystem.project.domain.events.DefaultProjectCreatedEvent;
+import com.kett.TicketSystem.project.domain.events.ProjectCreatedEvent;
 import com.kett.TicketSystem.project.repository.ProjectRepository;
 import com.kett.TicketSystem.user.application.dto.UserPostDto;
+import com.kett.TicketSystem.user.domain.User;
 import com.kett.TicketSystem.user.domain.events.UserCreatedEvent;
 import com.kett.TicketSystem.util.DummyEventListener;
 import org.junit.jupiter.api.AfterEach;
@@ -22,6 +26,7 @@ import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
+import javax.servlet.http.Cookie;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.UUID;
@@ -29,6 +34,7 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -47,6 +53,11 @@ public class ProjectControllerTests {
     private String userEmail;
     private String userPassword;
     private String jwt;
+
+    private String projectId;
+    private String projectName;
+    private String projectDescription;
+
 
 
     @Autowired
@@ -72,6 +83,9 @@ public class ProjectControllerTests {
         userEmail = "franz.stauffingen@gmail.com";
         userPassword = "MyLittlePony4Ever!";
 
+        projectName = "My Little Project";
+        projectDescription = "My very own project description";
+
         // post user
         UserPostDto userPostDto = new UserPostDto(userName, userEmail, userPassword);
         MvcResult userPostResult =
@@ -95,17 +109,57 @@ public class ProjectControllerTests {
         jwt = Objects.requireNonNull(postAuthenticationResult4.getResponse().getCookie("jwt")).getValue();
 
         dummyEventListener.deleteAllEvents(); // in buildUp to erase events of buildUp
+
+        // post Project
     }
 
     @AfterEach
     public void tearDown() {
+        jwt = null;
+        userId = null;
         userName = null;
         userEmail = null;
         userPassword = null;
-        userId = null;
-        jwt = null;
+
+        projectId = null;
+        projectName = null;
+        projectDescription = null;
 
         projectRepository.deleteAll();
+    }
+
+    @Test
+    public void postProjectTest() throws Exception {
+
+        // post project
+        ProjectPostDto projectPostDto = new ProjectPostDto(projectName, projectDescription);
+        MvcResult postResult =
+                mockMvc.perform(
+                                post("/projects")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(objectMapper.writeValueAsString(projectPostDto))
+                                        .cookie(new Cookie("jwt", jwt)))
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$.id").exists())
+                        .andExpect(jsonPath("$.name").value(projectPostDto.getName()))
+                        .andExpect(jsonPath("$.description").value(projectPostDto.getDescription()))
+                        .andReturn();
+        String postResponse = postResult.getResponse().getContentAsString();
+        projectId = JsonPath.parse(postResponse).read("$.id");
+        Project project = projectService.getProjectById(UUID.fromString(projectId));
+        assertEquals(UUID.fromString(projectId), project.getId());
+        assertEquals(projectPostDto.getName(), project.getName());
+        assertEquals(projectPostDto.getDescription(), project.getDescription());
+
+        // test ProjectCreatedEvent
+        Optional<ProjectCreatedEvent> event = dummyEventListener.getLatestProjectCreatedEvent();
+        assertTrue(event.isPresent());
+        Optional<ProjectCreatedEvent> emptyEvent = dummyEventListener.getLatestProjectCreatedEvent();
+        assertTrue(emptyEvent.isEmpty()); // check if only one event was thrown
+
+        ProjectCreatedEvent projectCreatedEvent = event.get();
+        assertEquals(UUID.fromString(projectId), projectCreatedEvent.getProjectId());
+        assertEquals(UUID.fromString(userId), projectCreatedEvent.getUserId());
     }
 
     @Test
@@ -135,5 +189,32 @@ public class ProjectControllerTests {
         Project defaultProject = projectService.getProjectById(defaultProjectCreatedEvent.getProjectId());
     }
 
+    /*
+    @Test
+    public void consumeLastMembershipDeletedEventTest() throws Exception {
+        eventPublisher.publishEvent(
+                new LastProjectMemberDeletedEvent(
+                        UUID.randomUUID(),
+                        projectId;
+                )
+        );
+
+        // TODO: find more stable alternative for testing
+        // shame: give services time to handle UserCreatedEvents
+        Thread.sleep(100);
+
+        // test DefaultProjectCreatedEvent
+        Optional<DefaultProjectCreatedEvent> event0 = dummyEventListener.getLatestDefaultProjectCreatedEvent();
+        assertTrue(event0.isPresent());
+        Optional<DefaultProjectCreatedEvent> emptyEvent0 = dummyEventListener.getLatestDefaultProjectCreatedEvent();
+        assertTrue(emptyEvent0.isEmpty()); // check if only one event was thrown
+
+        DefaultProjectCreatedEvent defaultProjectCreatedEvent = event0.get();
+        assertEquals(UUID.fromString(userId), defaultProjectCreatedEvent.getUserId());
+
+        // test if project was actually created
+        Project defaultProject = projectService.getProjectById(defaultProjectCreatedEvent.getProjectId());
+    }
+    */
 
 }
