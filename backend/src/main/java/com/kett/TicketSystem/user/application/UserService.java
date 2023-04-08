ce806a6624ba691fbc65f23b1a2ea134fb4dd996
dkettner <kettner_dennis@web.de>
2023-04-08 23:42:01 +0200

    chore(b-user): remove unused import

--- backend/src/main/java/com/kett/TicketSystem/user/application/UserService.java
+++ backend/src/main/java/com/kett/TicketSystem/user/application/UserService.java
@@ -19,7 +19,6 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
