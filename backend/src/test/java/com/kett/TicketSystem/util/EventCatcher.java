740af7ddba9226b754c2e57d3e913a4dfeeaab89
dkettner <kettner_dennis@web.de>
2023-04-12 22:43:48 +0200

    refactor(b-test): only check DomainEvents in EventCatcher

--- backend/src/test/java/com/kett/TicketSystem/util/EventCatcher.java
+++ backend/src/test/java/com/kett/TicketSystem/util/EventCatcher.java
@@ -33,8 +33,8 @@ public class EventCatcher {
     }
 
     @EventListener(condition = "@eventCatcher.isListening && @eventCatcher.isOfSpecifiedType(#newEvent)")
-    public void onApplicationEvent(Object newEvent) {
+    public void onApplicationEvent(DomainEvent newEvent) {
         isListening = false;
-        event = (DomainEvent) newEvent;
+        event = newEvent;
     }
 }
