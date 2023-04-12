54221f083e1340583ad873823afab3b332605d6e
dkettner <kettner_dennis@web.de>
2023-04-12 22:32:16 +0200

    feat(b-test): add EventCatcherTest

---
+++ backend/src/test/java/com/kett/TicketSystem/util/EventCatcherTest.java
@@ -0,0 +1,51 @@
+package com.kett.TicketSystem.util;
+
+import com.kett.TicketSystem.membership.domain.events.MembershipAcceptedEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+
+import java.util.UUID;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class EventCatcherTest {
+    private final EventCatcher eventCatcher;
+    private final ApplicationEventPublisher eventPublisher;
+
+    @Autowired
+    public EventCatcherTest(EventCatcher eventCatcher, ApplicationEventPublisher eventPublisher) {
+        this.eventCatcher = eventCatcher;
+        this.eventPublisher = eventPublisher;
+    }
+
+    @Test
+    public void eventCatcherTest() {
+
+        // should not get caught
+        MembershipAcceptedEvent event0 = new MembershipAcceptedEvent(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID());
+        eventPublisher.publishEvent(event0);
+
+        // start catching
+        eventCatcher.catchEventOfType(MembershipAcceptedEvent.class);
+
+        // no event of the specified type
+        assertNull(eventCatcher.getEvent());
+        assertNull(eventCatcher.getEvent());
+        assertNull(eventCatcher.getEvent());
+
+        // should get caught
+        MembershipAcceptedEvent event1 = new MembershipAcceptedEvent(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID());
+        eventPublisher.publishEvent(event1);
+
+        await().until(eventCatcher::hasCaughtEvent);    // fails if not caught within 10 seconds
+
+        // test if it is the same event
+        MembershipAcceptedEvent membershipAcceptedEvent = (MembershipAcceptedEvent) eventCatcher.getEvent();
+        assertNotEquals(event0, membershipAcceptedEvent);
+        assertEquals(event1, membershipAcceptedEvent);
+    }
+}
