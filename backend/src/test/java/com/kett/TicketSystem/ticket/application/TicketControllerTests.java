9f70e638b46703bd95050bceef62c77c1ea0856e
dkettner <kettner_dennis@web.de>
2023-04-14 18:08:01 +0200

    feat(b-test): add post ticket test

---
+++ backend/src/test/java/com/kett/TicketSystem/ticket/application/TicketControllerTests.java
@@ -0,0 +1,216 @@
+package com.kett.TicketSystem.ticket.application;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.kett.TicketSystem.membership.domain.Role;
+import com.kett.TicketSystem.membership.domain.State;
+import com.kett.TicketSystem.membership.repository.MembershipRepository;
+import com.kett.TicketSystem.phase.repository.PhaseRepository;
+import com.kett.TicketSystem.project.repository.ProjectRepository;
+import com.kett.TicketSystem.ticket.application.dto.TicketPostDto;
+import com.kett.TicketSystem.ticket.domain.Ticket;
+import com.kett.TicketSystem.ticket.domain.events.TicketCreatedEvent;
+import com.kett.TicketSystem.ticket.repository.TicketRepository;
+import com.kett.TicketSystem.user.repository.UserRepository;
+import com.kett.TicketSystem.util.EventCatcher;
+import com.kett.TicketSystem.util.RestRequestHelper;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.Cookie;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
+
+import static org.awaitility.Awaitility.await;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Transactional
+public class TicketControllerTests {
+    private final MockMvc mockMvc;
+    private final ObjectMapper objectMapper;
+    private final RestRequestHelper restMinion;
+    private final ApplicationEventPublisher eventPublisher;
+    private final EventCatcher eventCatcher;
+    private final TicketService ticketService;
+    private final TicketRepository ticketRepository;
+    private final UserRepository userRepository;
+    private final ProjectRepository projectRepository;
+    private final MembershipRepository membershipRepository;
+    private final PhaseRepository phaseRepository;
+
+    private String userName0;
+    private String userEmail0;
+    private String userPassword0;
+    private UUID userId0;
+    private String jwt0;
+
+    private String userName1;
+    private String userEmail1;
+    private String userPassword1;
+    private UUID userId1;
+    private String jwt1;
+
+    private String userName2;
+    private String userEmail2;
+    private String userPassword2;
+    private UUID userId2;
+    private String jwt2;
+
+    private String buildUpProjectName;
+    private String buildUpProjectDescription;
+    private UUID buildUpProjectId;
+
+    private UUID ticketId0;
+    private String ticketTitle0;
+    private String ticketDescription0;
+    private LocalDateTime dateOfTomorrow;
+
+    @Autowired
+    public TicketControllerTests(
+            MockMvc mockMvc,
+            ObjectMapper objectMapper,
+            ApplicationEventPublisher eventPublisher,
+            EventCatcher eventCatcher,
+            TicketService ticketService,
+            TicketRepository ticketRepository,
+            UserRepository userRepository,
+            ProjectRepository projectRepository,
+            MembershipRepository membershipRepository,
+            PhaseRepository phaseRepository
+    ) {
+        this.mockMvc = mockMvc;
+        this.objectMapper = objectMapper;
+        this.projectRepository = projectRepository;
+        this.restMinion = new RestRequestHelper(mockMvc, objectMapper);
+        this.eventPublisher = eventPublisher;
+        this.eventCatcher = eventCatcher;
+        this.ticketService = ticketService;
+        this.ticketRepository = ticketRepository;
+        this.userRepository = userRepository;
+        this.membershipRepository = membershipRepository;
+        this.phaseRepository = phaseRepository;
+    }
+
+    @BeforeEach
+    public void buildUp() throws Exception {
+        userName0 = "Geralt";
+        userEmail0 = "il.brucho@netflix.com";
+        userPassword0 = "DiesDasAnanasiospjefosias9999023";
+        userId0 = restMinion.postUser(userName0, userEmail0, userPassword0);
+        jwt0 = restMinion.authenticateUser(userEmail0, userPassword0);
+
+        userName1 = "Harry Potter";
+        userEmail1 = "harry.potter@hw.uk";
+        userPassword1 = "snapeShape88";
+        userId1 = restMinion.postUser(userName1, userEmail1, userPassword1);
+        jwt1 = restMinion.authenticateUser(userEmail1, userPassword1);
+
+        userName2 = "Ronald Weasley";
+        userEmail2 = "RonRonRonWeasley@hw.uk";
+        userPassword2 = "lkasjdfoijwaefo8238298";
+        userId2 = restMinion.postUser(userName2, userEmail2, userPassword2);
+        jwt2 = restMinion.authenticateUser(userEmail2, userPassword2);
+
+        buildUpProjectName = "toss a coin to your witcher";
+        buildUpProjectDescription = "50ct please";
+        buildUpProjectId = restMinion.postProject(jwt0, buildUpProjectName, buildUpProjectDescription);
+
+        UUID membershipId = restMinion.postMembership(jwt0, buildUpProjectId, userId1, Role.MEMBER);
+        restMinion.putMembershipState(jwt1, membershipId, State.ACCEPTED);
+
+        ticketTitle0 = "My first ticket";
+        ticketDescription0 = "do stuff";
+        dateOfTomorrow = LocalDateTime.now().plusDays(1);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        userName0 = null;
+        userEmail0 = null;
+        userPassword0 = null;
+        userId0 = null;
+        jwt0 = null;
+
+        userName1 = null;
+        userEmail1 = null;
+        userPassword1 = null;
+        userId1 = null;
+        jwt1 = null;
+
+        userName2 = null;
+        userEmail2 = null;
+        userPassword2 = null;
+        userId2 = null;
+        jwt2 = null;
+
+        buildUpProjectName = null;
+        buildUpProjectDescription = null;
+        buildUpProjectId = null;
+
+        ticketTitle0 = null;
+        ticketDescription0 = null;
+
+        ticketRepository.deleteAll();
+        userRepository.deleteAll();
+        phaseRepository.deleteAll();
+        projectRepository.deleteAll();
+        membershipRepository.deleteAll();
+    }
+
+    @Test
+    public void postTicketWithoutAssigneesTest() throws Exception {
+        eventCatcher.catchEventOfType(TicketCreatedEvent.class);
+        TicketPostDto ticketPostDto = new TicketPostDto(buildUpProjectId, ticketTitle0, ticketDescription0, dateOfTomorrow, new ArrayList<>());
+        MvcResult postResult0 =
+                mockMvc.perform(
+                                post("/tickets")
+                                        .contentType(MediaType.APPLICATION_JSON)
+                                        .content(objectMapper.writeValueAsString(ticketPostDto))
+                                        .cookie(new Cookie("jwt", jwt0)))
+                        .andExpect(status().isCreated())
+                        .andExpect(jsonPath("$.id").exists())
+                        .andExpect(jsonPath("$.projectId").value(buildUpProjectId.toString()))
+                        .andExpect(jsonPath("$.title").value(ticketTitle0))
+                        .andExpect(jsonPath("$.description").value(ticketDescription0))
+                        .andExpect(jsonPath("$.phaseId").isEmpty())
+                        .andExpect(jsonPath("$.creationTime").exists())
+                        .andExpect(jsonPath("$.dueTime").exists())
+                        .andReturn();
+        String postResponse0 = postResult0.getResponse().getContentAsString();
+        UUID ticketId = UUID.fromString(JsonPath.parse(postResponse0).read("$.id"));
+
+        // test event
+        await().until(eventCatcher::hasCaughtEvent);
+        TicketCreatedEvent ticketCreatedEvent = (TicketCreatedEvent) eventCatcher.getEvent();
+        assertEquals(ticketId, ticketCreatedEvent.getTicketId());
+        assertEquals(ticketPostDto.getProjectId(), ticketCreatedEvent.getProjectId());
+        assertEquals(userId0, ticketCreatedEvent.getUserId());
+
+        // test instance
+        Ticket ticket = ticketService.getTicketById(ticketId);
+        assertEquals(ticketId, ticket.getId());
+        assertEquals(ticketPostDto.getTitle(), ticket.getTitle());
+        assertEquals(ticketPostDto.getDescription(), ticket.getDescription());
+        assertEquals(ticketPostDto.getAssigneeIds(), ticket.getAssigneeIds());
+        assertEquals(ticketPostDto.getDueTime(), ticket.getDueTime());
+        assertTrue(ticket.getCreationTime().isBefore(LocalDateTime.now()));
+    }
+}
