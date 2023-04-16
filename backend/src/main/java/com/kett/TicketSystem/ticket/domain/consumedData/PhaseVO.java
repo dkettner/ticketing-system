26724ce7eb3ae39931927e1f80cb01d24ffbfc1c
dkettner <kettner_dennis@web.de>
2023-04-16 16:04:08 +0200

    chore(b-ticket): delete obsolete files

--- backend/src/main/java/com/kett/TicketSystem/ticket/domain/consumedData/PhaseVO.java
+++
@@ -1,5 +0,0 @@
-package com.kett.TicketSystem.ticket.domain.consumedData;
-
-import java.util.UUID;
-
-public record PhaseVO(UUID id, UUID previousPhaseId, UUID projectId) { }
--- backend/src/main/java/com/kett/TicketSystem/ticket/domain/consumedData/ProjectMembersVO.java
+++
@@ -1,6 +0,0 @@
-package com.kett.TicketSystem.ticket.domain.consumedData;
-
-import java.util.List;
-import java.util.UUID;
-
-public record ProjectMembersVO(UUID projectId, List<UUID> memberIds) { }
