7c659a3ec77bb973a436309337132bc00e31df04
dkettner <kettner_dennis@web.de>
2023-04-18 23:30:02 +0200

    feat(f-project): add detail view with delete option

---
+++ frontend/src/components/atomic-naive-ui/DeleteProjectButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+  import { ref, defineProps } from "vue";
+  import { NButton, NModal, NNotificationProvider } from 'naive-ui';
+  
+  import DeleteProjectForm from './DeleteProjectForm.vue';
+
+  const projectProps = defineProps(['project']);
+  const activateDeleteForm = ref(false);
+
+  function handleDeleteProjectButtonClicked() {
+    activateDeleteForm.value = true;
+  }
+  function handleCloseDeleteProjectForm() {
+    activateDeleteForm.value = false;
+  }
+</script>
+
+<template>
+
+    <n-modal v-model:show=activateDeleteForm :trap-focus="false">
+      <DeleteProjectForm 
+      :project="projectProps.project"
+        @closeProjectForm="handleCloseDeleteProjectForm" 
+      />
+    </n-modal>
+
+  <n-button @click="handleDeleteProjectButtonClicked" type="error" block error strong style="max-width: 125px">
+    Delete Project
+  </n-button>
+</template>
---
+++ frontend/src/components/atomic-naive-ui/DeleteProjectForm.vue
@@ -0,0 +1,69 @@
+<script setup>
+  import { useRouter } from 'vue-router';
+  import { ref, defineEmits, defineProps } from "vue";
+  import { useProjectStore } from "../../stores/project";
+  import { NForm, NButton, NGi, NInput, NFormItemGi, NGrid, useNotification } from "naive-ui";
+
+  const router = useRouter();
+  const projectStore = useProjectStore();
+  const notificationAgent = useNotification();
+  const emit = defineEmits(['closeProjectForm']);
+  const formRef = ref(null);
+  const projectProp = defineProps(['project']);
+
+  async function handleDeleteButtonClick(e) {
+    e.preventDefault();
+    formRef.value?.validate(async (errors) => {
+      if (!errors) {
+        const result = await projectStore.deleteProjectById(projectProp.project.id);
+        if (result.isDeleteSuccessful) {
+          sendNotification(
+            "Success", 
+            "Deleted project: " + projectProp.project.name
+          );
+          emit('closeProjectForm');
+          router.push('/projects/overview');
+        } else {
+          sendNotification("Error", result.message)
+        }
+      } else {
+        console.log(errors);
+        console.log("could not create new project");
+      }
+    });
+  };
+  function handleCancelButtonClick(e) {
+    emit('closeProjectForm');
+  };
+  function sendNotification(_title, _content) {
+    notificationAgent.create({
+      title: _title,
+      content: _content
+    });
+  }
+</script>
+
+<template>
+  <n-form
+    ref="formRef"
+    :size="medium"
+    label-placement="top"
+    style="min-width: 300px; width: 40%; max-width: 500px; background-color: #EEEEEE; padding: 20px;"
+  >
+    <n-grid :span="24" :x-gap="24" :cols ="1">
+      <n-gi :span="24">Do you really want to delete this project?</n-gi>
+      <n-gi> &nbsp; </n-gi>
+      <n-gi :span="24">
+        <div style="display: flex; justify-content: flex-end">
+          <n-button type="strong secondary" @click="handleCancelButtonClick">
+            Cancel
+          </n-button>
+          &nbsp;
+          <n-button type="error" @click="handleDeleteButtonClick">
+            Delete project
+          </n-button>
+        </div>
+      </n-gi>
+    </n-grid>
+  </n-form>
+</template>
