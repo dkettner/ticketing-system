df32a1342e91f9ca8232d929d49544cabb20cf37
dkettner <kettner_dennis@web.de>
2023-04-18 12:07:17 +0200

    feat(b-logging): log all ticket events

--- backend/src/main/java/com/kett/TicketSystem/common/logging/EventLogger.java
+++ backend/src/main/java/com/kett/TicketSystem/common/logging/EventLogger.java
@@ -9,7 +9,7 @@ import com.kett.TicketSystem.phase.domain.events.PhaseDeletedEvent;
 import com.kett.TicketSystem.project.domain.events.DefaultProjectCreatedEvent;
 import com.kett.TicketSystem.project.domain.events.ProjectCreatedEvent;
 import com.kett.TicketSystem.project.domain.events.ProjectDeletedEvent;
-import com.kett.TicketSystem.ticket.domain.events.TicketCreatedEvent;
+import com.kett.TicketSystem.ticket.domain.events.*;
 import com.kett.TicketSystem.user.domain.events.UserCreatedEvent;
 import com.kett.TicketSystem.user.domain.events.UserDeletedEvent;
 import com.kett.TicketSystem.user.domain.events.UserPatchedEvent;
@@ -29,36 +29,36 @@ public class EventLogger {
     public void handleMembershipAcceptedEvent(MembershipAcceptedEvent membershipAcceptedEvent) {
         logger.trace(
                 "membership accepted -> " +
-                "membershipId:" + membershipAcceptedEvent.getMembershipId() +
-                ", userId:" + membershipAcceptedEvent.getUserId() +
-                ", projectId:" + membershipAcceptedEvent.getProjectId()
+                        "membershipId:" + membershipAcceptedEvent.getMembershipId() +
+                        ", userId:" + membershipAcceptedEvent.getUserId() +
+                        ", projectId:" + membershipAcceptedEvent.getProjectId()
         );
     }
     @EventListener
     public void handleMembershipDeletedEvent(MembershipDeletedEvent membershipDeletedEvent) {
         logger.trace(
                 "membership deleted -> " +
-                "membershipId:" + membershipDeletedEvent.getMembershipId() +
-                ", userId:" + membershipDeletedEvent.getUserId() +
-                ", projectId:" + membershipDeletedEvent.getProjectId()
+                        "membershipId:" + membershipDeletedEvent.getMembershipId() +
+                        ", userId:" + membershipDeletedEvent.getUserId() +
+                        ", projectId:" + membershipDeletedEvent.getProjectId()
         );
     }
     @EventListener
     public void handleLastProjectMemberDeletedEvents(LastProjectMemberDeletedEvent lastProjectMemberDeletedEvent) {
         logger.trace(
                 "last membership deleted -> " +
-                "membershipId:" + lastProjectMemberDeletedEvent.getMembershipId() +
-                ", userId:" + lastProjectMemberDeletedEvent.getUserId() +
-                ", projectId:" + lastProjectMemberDeletedEvent.getProjectId()
+                        "membershipId:" + lastProjectMemberDeletedEvent.getMembershipId() +
+                        ", userId:" + lastProjectMemberDeletedEvent.getUserId() +
+                        ", projectId:" + lastProjectMemberDeletedEvent.getProjectId()
         );
     }
     @EventListener
     public void handleUnacceptedProjectMembershipCreatedEvent(UnacceptedProjectMembershipCreatedEvent unacceptedProjectMembershipCreatedEvent) {
         logger.trace(
                 "unaccepted membership created -> " +
-                "membershipId:" + unacceptedProjectMembershipCreatedEvent.getMembershipId() +
-                ", userId:" + unacceptedProjectMembershipCreatedEvent.getInviteeId() + ", " +
-                ", projectId:" + unacceptedProjectMembershipCreatedEvent.getProjectId()
+                        "membershipId:" + unacceptedProjectMembershipCreatedEvent.getMembershipId() +
+                        ", userId:" + unacceptedProjectMembershipCreatedEvent.getInviteeId() +
+                        ", projectId:" + unacceptedProjectMembershipCreatedEvent.getProjectId()
         );
     }
 
@@ -97,9 +97,46 @@ public class EventLogger {
     public void handleTicketCreatedEvent(TicketCreatedEvent ticketCreatedEvent) {
         logger.trace(
                 "ticket created -> " +
-                "ticketId:" + ticketCreatedEvent.getTicketId() +
-                ", userId:" + ticketCreatedEvent.getUserId() + ", " +
-                "projectId:" + ticketCreatedEvent.getProjectId()
+                        "ticketId:" + ticketCreatedEvent.getTicketId() +
+                        ", userId:" + ticketCreatedEvent.getUserId() +
+                        ", projectId:" + ticketCreatedEvent.getProjectId()
+        );
+    }
+    @EventListener
+    public void handleTicketDeletedEvent(TicketDeletedEvent ticketDeletedEvent) {
+        logger.trace(
+                "ticket deleted -> " +
+                        "ticketId:" + ticketDeletedEvent.getTicketId() +
+                        ", projectId:" + ticketDeletedEvent.getProjectId() +
+                        ", phaseId:" + ticketDeletedEvent.getPhaseId()
+        );
+    }
+    @EventListener
+    public void handleTicketAssignedEvent(TicketAssignedEvent ticketAssignedEvent) {
+        logger.trace(
+                "ticket assigned -> " +
+                        "ticketId:" + ticketAssignedEvent.getTicketId() +
+                        ", userId:" + ticketAssignedEvent.getAssigneeId() +
+                        ", projectId:" + ticketAssignedEvent.getProjectId()
+        );
+    }
+    @EventListener
+    public void handleTicketUnassignedEvent(TicketUnassignedEvent ticketUnassignedEvent) {
+        logger.trace(
+                "ticket assigned -> " +
+                        "ticketId:" + ticketUnassignedEvent.getTicketId() +
+                        ", userId:" + ticketUnassignedEvent.getAssigneeId() +
+                        ", projectId:" + ticketUnassignedEvent.getProjectId()
+        );
+    }
+    @EventListener
+    public void handleTicketPhaseUpdatedEvent(TicketPhaseUpdatedEvent ticketPhaseUpdatedEvent) {
+        logger.trace(
+                "ticket assigned -> " +
+                        "ticketId:" + ticketPhaseUpdatedEvent.getTicketId() +
+                        ", oldPhaseId:" + ticketPhaseUpdatedEvent.getOldPhaseId() +
+                        ", newPhaseId:" + ticketPhaseUpdatedEvent.getNewPhaseId() +
+                        ", projectId:" + ticketPhaseUpdatedEvent.getProjectId()
         );
     }
 
@@ -110,27 +147,27 @@ public class EventLogger {
     public void handleUserCreatedEvent(UserCreatedEvent userCreatedEvent) {
         logger.trace(
                 "user created -> " +
-                "userId:" + userCreatedEvent.getUserId() +
-                ", name:" + userCreatedEvent.getName() +
-                ", email:" + userCreatedEvent.getEmailAddress()
+                        "userId:" + userCreatedEvent.getUserId() +
+                        ", name:" + userCreatedEvent.getName() +
+                        ", email:" + userCreatedEvent.getEmailAddress()
         );
     }
     @EventListener
     public void handleUserDeletedEvent(UserDeletedEvent userDeletedEvent) {
         logger.trace(
                 "user deleted -> " +
-                "userId:" + userDeletedEvent.getUserId() +
-                ", name:" + userDeletedEvent.getName()+
-                ", email:" + userDeletedEvent.getEmailAddress()
+                        "userId:" + userDeletedEvent.getUserId() +
+                        ", name:" + userDeletedEvent.getName()+
+                        ", email:" + userDeletedEvent.getEmailAddress()
         );
     }
     @EventListener
     public void handleUserPatchedEvent(UserPatchedEvent userPatchedEvent) {
         logger.trace(
                 "user patched -> " +
-                "userId:" + userPatchedEvent.getUserId() +
-                ", name:" + userPatchedEvent.getName()+
-                ", email:" + userPatchedEvent.getEmailAddress()
+                        "userId:" + userPatchedEvent.getUserId() +
+                        ", name:" + userPatchedEvent.getName()+
+                        ", email:" + userPatchedEvent.getEmailAddress()
         );
     }
 }
