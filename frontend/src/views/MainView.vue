972b403a39c7aeb06f9f7b26b835f2ad544f804c
dkettner <kettner_dennis@web.de>
2023-04-23 15:56:49 +0200

    chore(f): reformat code

--- frontend/src/views/MainView.vue
+++ frontend/src/views/MainView.vue
@@ -1,32 +1,32 @@
 <script setup>
-  import { onMounted } from 'vue';
-  import { storeToRefs } from 'pinia';
-  import { useUserStore } from '../stores/user.js';
-  import { useSessionStore } from '../stores/session';
-  import { useProjectStore } from '../stores/project';
-  import { useMembershipStore } from '../stores/membership';
-  import { usePhaseStore } from '../stores/phase';
-  import { NSpace, NLayout, NLayoutHeader, NNotificationProvider, NDivider} from 'naive-ui';
+import { onMounted } from 'vue';
+import { storeToRefs } from 'pinia';
+import { useUserStore } from '../stores/user.js';
+import { useSessionStore } from '../stores/session';
+import { useProjectStore } from '../stores/project';
+import { useMembershipStore } from '../stores/membership';
+import { usePhaseStore } from '../stores/phase';
+import { NSpace, NLayout, NLayoutHeader, NNotificationProvider, NDivider } from 'naive-ui';
 
-  import SidebarMenuVue from '../components/atomic-naive-ui/SidebarMenu.vue';
-  import UserMenu from '../components/atomic-naive-ui/UserMenu.vue';
+import SidebarMenuVue from '../components/atomic-naive-ui/SidebarMenu.vue';
+import UserMenu from '../components/atomic-naive-ui/UserMenu.vue';
 
-  const sessionStore = useSessionStore();
-  const membershipStore = useMembershipStore();
-  const projectStore = useProjectStore();
-  const phasestore = usePhaseStore();
-  const userStore = useUserStore();
-  const { projects } = storeToRefs(projectStore)
-  const { user } = storeToRefs(userStore);
+const sessionStore = useSessionStore();
+const membershipStore = useMembershipStore();
+const projectStore = useProjectStore();
+const phasestore = usePhaseStore();
+const userStore = useUserStore();
+const { projects } = storeToRefs(projectStore)
+const { user } = storeToRefs(userStore);
 
-  onMounted( async () => {
-    userStore.setEmail(sessionStore.email);
-    await userStore.updateUserByEmail();
-    await membershipStore.updateMembershipsByEmail();
-    await projectStore.updateProjectsByAcceptedMemberships();
-    const projectIds = projects.value.map(element => element.id);
-    projectIds.forEach( async (projectId) => await phasestore.updatePhasesByProjectId(projectId))
-  });
+onMounted(async () => {
+  userStore.setEmail(sessionStore.email);
+  await userStore.updateUserByEmail();
+  await membershipStore.updateMembershipsByEmail();
+  await projectStore.updateProjectsByAcceptedMemberships();
+  const projectIds = projects.value.map(element => element.id);
+  projectIds.forEach(async (projectId) => await phasestore.updatePhasesByProjectId(projectId))
+});
 </script>
 
 <template>
@@ -41,14 +41,17 @@
       <UserMenu />
     </div>
   </div>
-  <n-notification-provider>
-    <n-space vertical>
-      <n-layout>
-        <n-layout has-sider>
-          <SidebarMenuVue />
-          <RouterView />
+  <div>
+    <n-notification-provider>
+      <n-space vertical>
+        <n-layout>
+          <n-layout has-sider>
+            <SidebarMenuVue />
+
+            <RouterView />
+          </n-layout>
         </n-layout>
-      </n-layout>
-    </n-space>
-  </n-notification-provider>
+      </n-space>
+    </n-notification-provider>
+  </div>
 </template>
--- frontend/src/views/ProjectsView.vue
+++ frontend/src/views/ProjectsView.vue
@@ -1,42 +1,64 @@
 <script setup>
   import { useProjectStore } from '../stores/project';
-  import { useMembershipStore } from '../stores/membership';
   import { storeToRefs } from 'pinia';
-  import { NCard, NEllipsis, NRow, NCol } from 'naive-ui';
+  import { NCard, NEllipsis, NRow, NCol, NEmpty, NIcon } from 'naive-ui';
+  import {
+    AlbumsOutline as ProjectsIcon
+  } from "@vicons/ionicons5";
 
   import NewProjectButton from '../components/atomic-naive-ui/NewProjectButton.vue';
 
-  const membershipStore = useMembershipStore();
-  const memberships = storeToRefs(membershipStore);
   const projectStore = useProjectStore();
   const { projects } = storeToRefs(projectStore);
 </script>
 
 <template>
-  <div style="padding-left: 25px;">
+  <div style="padding-left: 25px; width: 75%;">
     <h1>Projects</h1>
     <NewProjectButton />
     <br>
-    <n-row :gutter="[10, 10]">
-      <div v-for="project in projects">
-        <n-col :span="100">
-          <RouterLink :to="`/projects/${project.id}`" style="text-decoration: none">
-            <n-card :title="project.name" embedded hoverable style="width: 300px; height: 250px;">
-              <n-ellipsis line-clamp="3">
-                {{ project.description }}
-                <template #tooltip>
-                  <div style="text-align: center; max-width: 300px; max-height: 100px;">
-                    {{ project.description }}
-                  </div>
+    <div class="noProjectsPlaceholder" v-if="projects.length == 0">
+      <n-empty size="huge" description="You can't find anything">
+        <template #icon>
+          <n-icon>
+            <ProjectsIcon />
+          </n-icon>
+        </template>
+        No Projects Found
+      </n-empty>
+    </div>
+    <div v-else>
+      <n-row :gutter="[10, 10]">
+        <div v-for="project in projects">
+          <n-col :span="100">
+            <RouterLink :to="`/projects/${project.id}`" style="text-decoration: none">
+              <n-card :title="project.name" embedded hoverable style="width: 300px; height: 250px;">
+                <n-ellipsis line-clamp="3">
+                  {{ project.description }}
+                  <template #tooltip>
+                    <div style="text-align: center; max-width: 300px; max-height: 100px;">
+                      {{ project.description }}
+                    </div>
+                  </template>
+                </n-ellipsis>
+                <template #footer>
+                  <p style="font-style: italic;">creation time: {{ new Date(project.creationTime).toLocaleString() }}</p>
                 </template>
-              </n-ellipsis>
-              <template #footer>
-                <p style="font-style: italic;">creation time: {{ new Date(project.creationTime).toLocaleString() }}</p>
-              </template>
-            </n-card>
-          </RouterLink>
-        </n-col>
-      </div>
-    </n-row>
+              </n-card>
+            </RouterLink>
+          </n-col>
+        </div>
+      </n-row>
+    </div>
   </div>
 </template>
+
+<style>
+  .noProjectsPlaceholder {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    min-height: 50vh;
+  }
+</style>
