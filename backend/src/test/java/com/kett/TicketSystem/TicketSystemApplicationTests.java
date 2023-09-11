386ea3258e56c7d1184bd30c5c18e99cd4a8c965
dkettner <kettner_dennis@web.de>
2023-09-11 17:52:27 +0200

    fix(b-test): use test profile in ApplicationTests

--- backend/src/test/java/com/kett/TicketSystem/TicketSystemApplicationTests.java
+++ backend/src/test/java/com/kett/TicketSystem/TicketSystemApplicationTests.java
@@ -2,8 +2,10 @@ package com.kett.TicketSystem;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles({ "test" })
 class TicketSystemApplicationTests {
 
 	@Test
